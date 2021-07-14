116e214da36d7629137fc1b3a01610f3a6986ebf
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-07-14 16:32:46 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ int main()
 	for (int i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			cout << "Fizz\n";
+			cout << "FizzBuzz\n";
 		else if (i % 3 == 0)
 			cout << "Fizz\n";
 		else if ((i % 5 == 0))
